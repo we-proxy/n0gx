ba6af71f8d156b9c2e1f2f3437daab1997d4ae7e
Fritz Lin <uxfritz@163.com>
2017-07-02 08:31:32 +0800

    add https support https://github.com/Neilpang/acme.sh/wiki/How-to-issue-a-cert#3-standalone-tls-mode-1 https://stackoverflow.com/questions/11744975/enabling-https-on-express-js https://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express https://stackoverflow.com/questions/31673587/error-unable-to-verify-the-first-certificate-in-nodejs https://www.npmjs.com/package/ssl-root-cas

--- bin/n0gx.cli.js
+++ bin/n0gx.cli.js
@@ -1,16 +1,49 @@
 #!/usr/bin/env node
 var n0gx = require('../')
 var filepath = process.argv[2]
-var port = +process.argv[3]
+var httpPort = +process.argv[3] || 80
+var httpsPort = +process.argv[4] || 443
 var path = require('path')
 
 if (!filepath) throw new Error('conf path required')
-if (!port) throw new Error('port required')
+if (!httpPort) throw new Error('httpPrt required')
+if (!httpsPort) throw new Error('httpsPort required')
 var cwd = process.cwd()
 var conf = require(path.resolve(cwd, filepath))
 
-var app = n0gx(conf)
-var server = app.listen(port, function(e){
-  if (e) throw e
-  console.log('started on', server.address().port)
+var express = require('express')
+var httpApp = express()
+var httpsApp = n0gx(conf)
+
+httpApp.get('*', function (req, res) {
+  var httpsUrl = `https://${req.get('host')}${req.url}`
+  res.redirect(301, httpsUrl)
 })
+
+var fs = require('fs')
+var http = require('http')
+var https = require('https')
+
+var domain = 'fritx.me'
+var basedir = '/root/.acme.sh'
+
+var keyfile = `${basedir}/${domain}/${domain}.key`
+// var certfile = `${basedir}/${domain}/${domain}.cer`
+var certfile = `${basedir}/${domain}/fullchain.cer`
+var key  = fs.readFileSync(keyfile, 'utf8')
+var cert = fs.readFileSync(certfile, 'utf8')
+var credentials = { key, cert }
+
+//var httpServer = http.createServer(httpApp)
+var httpServer = http.createServer(httpsApp)
+var httpsServer = https.createServer(credentials, httpsApp)
+
+httpServer.listen(httpPort, function (e) {
+  if (e) throw e
+  console.log('started on', httpServer.address().port)
+})
+httpsServer.listen(httpsPort, function (e) {
+  if (e) throw e
+  console.log('started on', httpsServer.address().port)
+})
+
